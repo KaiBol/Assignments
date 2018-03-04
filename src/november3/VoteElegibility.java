53bd07b19749e7af44a1d961fa5d5486da4dbd77
yrysb <yrysb@DESKTOP-NJKPHTB>
2018-03-04 15:54:11 -0800

    jsdjsd

---
+++ src/november3/VoteElegibility.java
@@ -0,0 +1,20 @@
+package november3;
+
+import java.util.Scanner;
+
+public class VoteElegibility {
+	//nov25
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please, enter your age:" );
+		int age=input.nextInt();
+		if (age >= 18) {
+			System.out.println("You are eligible to vote");
+			
+		}
+		else 
+			System.out.println("You are not eligible to vote");
+	}
+	
+
+}
