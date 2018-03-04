24f55744e405afbe3241492525605339f1eddeaa
yrysb <yrysb@DESKTOP-NJKPHTB>
2018-03-04 15:53:13 -0800

    fvfhghg

---
+++ src/december2/WordsLengthFour.java
@@ -0,0 +1,18 @@
+package december2;
+
+public class WordsLengthFour {
+
+	public static void main(String[] args) {
+		
+		String name [] = { "Erik", "Lisa", "Jose", "Roger", "Nina", "Ekaterina", "Peter", "Sara"};
+			
+		for (int i = 0; i<name.length; i++) {
+		    if (name[i].length()==4) {
+			String nameLengthFour [] = { name[i] };
+		for ( String SuperMan : nameLengthFour ) {
+			System.out.println(SuperMan);
+		}	
+		}
+		}
+		}
+		}
