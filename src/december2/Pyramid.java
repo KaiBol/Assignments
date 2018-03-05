8f7bee99187ab57a781d6b60b69cfbd0dadcb615
yrysb <yrysb@DESKTOP-NJKPHTB>
2018-03-04 16:00:50 -0800

    jsajkdhsa

---
+++ src/december2/Pyramid.java
@@ -0,0 +1,27 @@
+package december2;
+
+
+		
+    public class Pyramid {
+
+	public static void main(String[] args) {
+				System.out.println("Pyramid pattern of star in Java : ");
+				
+	
+				for (int i = 0; i < 4; i++) { 
+					for (int j = i; j < 4; j++) 
+						System.out.print(" ");
+				for (int k = 0; k <= (i*2); k++) 	
+					System.out.print("*");
+						 
+					
+				System.out.println(); 
+	} 
+
+	} 
+	
+	}	
+	
+	
+
+
